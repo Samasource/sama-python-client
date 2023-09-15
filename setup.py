f8dec8a2bf17fd9dc8e4cf2666d592b766d82b95
Edward Chan <75637964+edchan7-sama@users.noreply.github.com>
2023-09-15 16:07:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='sama',
-        version='0.5.10',
+        version='0.5.11',
         description='Sama Python Client and Databricks Connector',
         long_description=long_description,
         long_description_content_type="text/markdown",
         url='https://www.sama.com',
         author='Edward',
         author_email='echan@samasource.org',
-        license='MIT',
+        license=' Apache-2.0 license',
         packages=find_packages(),
         install_requires=[
             'retry',
